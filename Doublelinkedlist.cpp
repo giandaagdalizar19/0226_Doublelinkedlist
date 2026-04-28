f6d4a6a955d191bb2d0504e32746c79d4c0636c8
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-04-28 13:18:25 +0700

    class doublelinkedlist

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -11,3 +11,21 @@ public:
     Node *prev;
 };
 
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }
+};       
